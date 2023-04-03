3161440151b3825ed77bda82d32f443fd7d79d41
eesha1604 <100568175+eesha1604@users.noreply.github.com>
2023-04-03 23:37:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,36 +85,196 @@ resource "aws_acm_certificate" "ssl_certificate" {
 }
 
 # Create Elastic Load Balancer (ELB)
-resource "aws_elb" "load_balancer" {
-  name               = "my-load-balancer"
-  subnets            = var.elb_subnets
-  security_groups    = [var.elb_security_group]
-  idle_timeout       = 400
-  connection_draining = true
-  connection_draining_timeout = 300
+# resource "aws_elb" "load_balancer" {
+#   name               = "my-load-balancer"
+#   subnets            = var.elb_subnets
+#   security_groups    = [var.elb_security_group]
+#   idle_timeout       = 400
+#   connection_draining = true
+#   connection_draining_timeout = 300
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
+#   listener {
+#     instance_port     = 80
+#     instance_protocol = "HTTP"
+#     lb_port           = 80
+#     lb_protocol       = "HTTP"
+#   }
+
+#   listener {
+#     instance_port      = 80
+#     instance_protocol  = "HTTP"
+#     lb_port            = 443
+#     lb_protocol        = "HTTPS"
+#     ssl_certificate_id = aws_acm_certificate.ssl_certificate.id
+#   }
+# }
+
+data "aws_ami" "myami" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  listener {
-    instance_port      = 80
-    instance_protocol  = "HTTP"
-    lb_port            = 443
-    lb_protocol        = "HTTPS"
-    ssl_certificate_id = aws_acm_certificate.ssl_certificate.id
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
+
+# Creating Key-pair
+resource "aws_key_pair" "tf-key" {
+  key_name   = "terraform-keypair"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+# Create   EC2-Instance
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.myami.id
+  instance_type          = var.minikube_type
+  key_name               = aws_key_pair.tf-key.key_name
+  vpc_security_group_ids = [aws_security_group.allow-rule.id]
+  subnet_id              = aws_subnet.public_subnet.id
+  user_data              = file("script.sh")
+
+  tags = {
+    Name = "ec2_instance"
+  }
+}
+
+# Create Internet-Gateway
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "FinalProject-IGW"
+  }
+}
+
+
+# Create public RouteTable
+resource "aws_route_table" "public_routetable" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0" //use so subnet can connect to anywhere
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+
+  tags = {
+    Name = "FinalProject-Public_RouteTable"
+  }
+}
+
+# Associate Public Subnet in->Public Route table
+resource "aws_route_table_association" "Publicsubnet-associate" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_routetable.id
+}
+
+resource "aws_route53_zone" "example" {
+  name = "devops-challenge-dev"
+}
+
+
+resource "aws_route53_record" "example" {
+  name    = "example.com"
+  type    = "A"
+  zone_id = aws_route53_zone.example.zone_id
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn = aws_acm_certificate.example.arn
+
+  timeouts {
+    create = "30m"
+  }
+
+  depends_on = [aws_route53_record.example]
+}
+
+# Create Security Group
+resource "aws_security_group" "allow-rule" {
+  name        = "allow_rule"
+  description = "Allow inbound traffic"
+
+  vpc_id = aws_vpc.myvpc.id
+
+  dynamic "ingress" {
+    for_each = var.port
+    iterator = port_number
+    content {
+
+      description = "Allow Port ${port_number.value}"
+      from_port   = port_number.value
+      to_port     = port_number.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-terraform-sg"
+  }
+}
+
+# Create VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "FinalProject_VPC"
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.public_subnets_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true //use to make the subnet public
+  tags = {
+    Name = "FinalProject-PublicSubnet"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.private_subnets_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = false //use to make the subnet private
+  tags = {
+    Name = "FinalProject-PrivateSubnet"
+  }
+}
+
 
 # Create EC2 instances for each environment
-resource "aws_instance" "web_instances" {
-  count = length(var.environments)
+# resource "aws_instance" "web_instances" {
+#   count = length(var.environments)
 
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-}
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
+#   subnet_id     = var.subnet_id
+# }
